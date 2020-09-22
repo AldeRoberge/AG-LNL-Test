bc00688c68e23ee61c9069b7e482919cb4a47d79
Alde Roberge <alderoberge@gmail.com>
2020-09-21 22:51:29 -0400

    Removed final chees

--- Assets/Game/Scripts/Interaction/Grabbable.cs
+++ Assets/Game/Scripts/Interaction/Grabbable.cs
@@ -13,8 +13,8 @@ namespace DefaultNamespace
 
         private GameObject GrabbedBy;
 
-
         private Color cachedColor;
+
 
         public void Awake()
         {
@@ -27,12 +27,12 @@ namespace DefaultNamespace
             GrabbingSphere.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
             GrabbingSphere.transform.SetParent(transform, false);
             GrabbingSphere.transform.position = new Vector3(0, 0.5f, 0);
-            
+
             if (!Config.Debug)
             {
                 GrabbingSphere.GetComponent<MeshRenderer>().enabled = false;
             }
-            
+
             cachedColor = GetComponentInChildren<MeshRenderer>().material.color;
         }
 
